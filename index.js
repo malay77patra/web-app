33a3ae1512ad8de862ac2a4516ea209b835c40a7
malay77patra <malay77patra@gmail.com>
2022-06-22 21:25:19 +0530

    port fixed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/about', function (req, res) {
   res.send("the about page")
 });
 //server
-var server = app.listen(8080, function () {
+var server = app.listen(process.env.PORT || 8080, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('app listening at http://%s:%s', host, port);  
