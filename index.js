8d9f6da1394f1bbd6ff5ebd369df5eb057ba845e
malay77patra <malay77patra@gmail.com>
2022-06-22 18:58:45 +0530

    html page added

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hemlo worlmd!");
+const express = require('express');
+const app = express();
+const path = require('path');
+//app functionality
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname+'/index.html'))
+});
+app.get('/about', function (req, res) {
+  res.send("the about page")
+});
+//server
+var server = app.listen(8080, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('app listening at http://%s:%s', host, port);  
+});
